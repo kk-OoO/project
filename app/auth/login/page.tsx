6cc146ffedf606cc726af3264014ed594963cb9d
kk-OoO <UoxoU_325@outlook.jp>
2025-08-28 20:53:27 +0900

    login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,12 +2,28 @@ import { login } from "../actions";
 
 export default function Login() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-    </form>
+    <div className="bg-gray-200 flex w-4xl mx-auto  p-20 mt-10 justify-between">
+      <div className="flex flex-col items-center p-5">
+        <h1 className="text-center m-5">ログイン</h1>
+        <input
+          type="text"
+          placeholder="aaa@aaa"
+          className="border-b-2 m-2"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border-b-2 m-2"
+
+        />
+        <button className="w-30 h-30 rounded-full border flex items-center justify-center">ログイン</button>
+      </div>
+      <div className="p-5">
+        <h1 className="text-center m-5 mb-25">新規登録</h1>
+        <div>
+          <button className="w-30 h-30 rounded-full border flex items-center justify-center" formAction={login}>こちらから</button>
+        </div>
+      </div>
+    </div>
   );
 }
