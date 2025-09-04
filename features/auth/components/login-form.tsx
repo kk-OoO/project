561bf8678bf652217b12a2e72a48212e61042fae
GigaOwO <takumagiga1118@gmail.com>
2025-09-05 00:50:54 +0900

    loginとsignupのデザインを修正

--- features/auth/components/login-form.tsx
+++ features/auth/components/login-form.tsx
@@ -3,37 +3,41 @@ import { login } from "../fetchers";
 
 export default function LoginForm() {
   return (
-    <div className="bg-gray-200 flex w-4xl mx-auto  p-20 mt-10 justify-between">
+    <div className="bg-gray-200 flex max-w-4xl mx-auto p-20 mt-10 justify-center gap-48">
       <div className="flex flex-col items-center p-5">
-        <h1 className="text-center m-5">ログイン</h1>
-        <form action={login}>
+        <h1 className="text-center m-5 border-b border-black text-2xl mt-6">
+          ログイン
+        </h1>
+        <form action={login} className="flex flex-col items-center">
           <input
             id="email"
             name="email"
             type="email"
             placeholder="aaa@aaa"
-            className="border-b-2 m-2"
+            className="border-b border-b-black m-2 p-2 bg-transparent focus:outline-none"
           />
           <input
             id="password"
             name="password"
             type="password"
             placeholder="password"
-            className="border-b-2 m-2"
+            className="border-b border-b-black m-2 p-2 bg-transparent focus:outline-none"
           />
           <button
-            className="w-30 h-30 rounded-full border flex items-center justify-center"
-            type="submit"
+            className="w-32 h-32 rounded-full border border-black flex items-center justify-center mt-4 transition-colors"
+            formAction={login}
           >
             ログイン
           </button>
         </form>
       </div>
       <div className="p-5">
-        <h1 className="text-center m-5 mb-25">新規登録</h1>
+        <h1 className="text-center m-5 mb-38 border-b border-black text-2xl">
+          新規登録
+        </h1>
         <div>
           <Link
-            className="w-30 h-30 rounded-full border flex items-center justify-center"
+            className="w-32 h-32 rounded-full border border-black flex items-center justify-center transition-colors"
             href={"/auth/signup"}
           >
             こちらから
